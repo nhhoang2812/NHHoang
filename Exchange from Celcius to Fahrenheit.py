9c20ec8da67073bb2f00d64f6d5be2d4334d8d98
nhhoang2812 <95356649+nhhoang2812@users.noreply.github.com>
2021-12-08 15:55:25 +0700

    Add files via upload

---
+++ Exchange from Celcius to Fahrenheit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+for i in range (1000):
+    c = input("Enter temperature in Celcius: ")
+    f = 9*float(c)/5+32
+    print("Temperature in Fahrenheit is: ",f)
+   
+
